a3d6f2db056fa5745c364d1dd844b0daeb02f6de
Roman <brysommer1@gmail.com>
2023-05-24 14:57:57 +0300

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { anketaListiner } from './anketa.js';
 import { dataBot } from './values.js';
-import { postingLots } from './postingLot.js';
 
 const bot = new TelegramBot(dataBot.telegramBotToken, { polling: true });
-const admin = new TelegramBot(dataBot.adminBot, { polling: true });
-export { bot, admin };
+
+export { bot };
 
 anketaListiner();
-postingLots();
