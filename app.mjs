61177844ca6331beb0becae5abbff34c92c79870
Roman <brysommer1@gmail.com>
2023-06-15 15:14:45 +0300

    lalla

--- app.mjs
+++ app.mjs
@@ -1,7 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { anketaListiner } from './anketa.mjs';
 import { dataBot } from './values.mjs';
-import {decodeQR} from './qrdecode.mjs';
+import { decodeQR } from './qrdecode.mjs';
 import { sequelize } from './models/sequelize.mjs';
 import { logger } from './logger/index.mjs';
 
